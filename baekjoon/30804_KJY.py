132a22d807ee15c2f8747b432a3d27c1e1d5b2e9
김지윤 <zbqlxm5484@naver.com>
2026-02-12 14:51:16 +0900

    solve

--- baekjoon/30804_KJY.py
+++ baekjoon/30804_KJY.py
@@ -6,22 +6,3 @@ arr = [0]*10
 kind = 0
 max_val = 0
 for right in range(N):
-    if arr[strick[right]] == 0 :
-        kind += 1
-    arr[strick[right]] += 1
-
-    while kind > 2 :
-        t = strick[left]
-
-        arr[t] -= 1
-        if arr[t] == 0 :
-            kind -= 1
-        
-        left += 1
-    
-    length = right-left + 1
-    if length > max_val :
-        max_val = length
-
-    
-print(max_val) 
